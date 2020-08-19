5226ba2b8b2f204a4d422c1955f0015bf6e046ef
henryer4 <69907801+henryer4@users.noreply.github.com>
2020-08-19 11:05:56 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/*****************************************
+* hello.java
+****************************************
+Elizabeth Henry
+8/16/2020
+CMSC255-004
+*/
+public class Hello {
+	public static void main(String[] args) {
+	System.out.println("Elizabeth Henry");
+	System.out.println("Hello.java");
+	System.out.println("Lab 1");
+	System.out.println("Aug 16, 2020");
+	System.out.println("CMSC255-004");
+	System.out.println();
+	}
+}
